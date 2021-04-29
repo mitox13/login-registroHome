0efd94a2744403598f23a223b4f961574113275a
Jaime <ramoslaurente13@gmail.com>
2021-04-28 21:22:47 -0500

    agregando headerCliente

--- src/modules/cliente/components/HeaderCliente.js
+++ src/modules/cliente/components/HeaderCliente.js
@@ -1,11 +1,52 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import AuthContext from '../../../context/authContext'
 const HeaderCliente = () => {
+
+    const history = useHistory()
+    const {clientelogueado} = useContext(AuthContext)
     return (
-     <>
-            <header>nombre cliente nosotros contactenos</header>
-            <h1>HEADER CLIENTE</h1>
-       </>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-4 navbar__clientelogueado" >
+                <NavLink className="navbar-brand" to='/cliente/logueado'>MY FIT <span className='text-warning'>TEAM</span></NavLink>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <span className="nav-link text-center" style={{fontSize:'20px'}}>{clientelogueado.primerNombre + '  ' +clientelogueado.apellidoPaterno}</span>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link text-white btn ml-3 mr-3" exact activeClassName="active bg-warning"
+                                to="/cliente/logueado">INICIO</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link text-white btn ml-3 mr-3" exact activeClassName="active "
+                                to="/cliente/logueado">NOSOTROS</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link text-white btn ml-3 mr-3" exact activeClassName="active "
+                                to="/cliente/logueado">CONTACTENOS</NavLink>
+                        </li>
+
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0 ">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+                        <button
+                            className="btn btn-warning my-2 my-sm-0 ml-2"
+                            onClick={() => {
+                                localStorage.removeItem("user")
+                                history.push('/')
+                            }}
+                        >Cerrar Sesion</button>
+                    </form>
+                </div>
+            </nav>
+        </>
     )
 }
 
--- src/modules/cliente/pages/PantallaPricincipalClienteLogueado/PantallaPrincipalClienteLogueado.js
+++ src/modules/cliente/pages/PantallaPricincipalClienteLogueado/PantallaPrincipalClienteLogueado.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AuthContext from '../../../../context/authContext'
+
 
 const PantallaPrincipalClienteLogueado = () => {
+    
+    const {clientelogueado} = useContext(AuthContext)
     return (
-        <div>
-            <h1>NUESTRAS ESPECIALIDADES </h1>
-            <h1>NUESTROS PROFESIONALES</h1>
+        <div className='container text-center'>
+            <br/>
+            <h1 className='text-center'>  PANTALLA PRINCIPAL CLIENTE LOGUEADO</h1>
+            <br/>
+            <br/>
+            <h2>NUESTRAS ESPECIALIDADES </h2>
+            <br/>
+            <br/>
+            <h2>Nombre(s)</h2>
+            <h4>{clientelogueado.primerNombre +'  '}{clientelogueado.segundoNombre}</h4>
+            <h2>Apellidos</h2>
+            <h4>{clientelogueado.apellidoPaterno + '  ' } {clientelogueado.apellidoMaterno}</h4>
+            <h2>Celular</h2>
+            <h4>{clientelogueado.celular}</h4>
+            <h2>Correo</h2>
+            <h4>{clientelogueado.correo}</h4>
+            <br/>
+            <br/>
+            <h2>NUESTROS PROFESIONALES</h2>
         </div>
     )
 }
