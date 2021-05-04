2ac7b40ad0173ec92e8faae1c5ff39af92d5ef1e
mito <mito@gmail.com>
2021-05-04 18:31:33 -0500

    login-registro-nosotros-listaProfesionales del modulo HOME listos ahora si creo xd

--- src/modules/home/pages/Nosotros/NosotrosHome.js
+++ src/modules/home/pages/Nosotros/NosotrosHome.js
@@ -6,14 +6,16 @@ const NosotrosHome = () => {
     return (
         <main className='container-fluid   '>
 
-            <div className="row nosotros__container pt-3 ml-2 mr-2 d-flex align-items-center">
+            <div className="row nosotros__container pt-3 ml-2 mr-2 d-flex align-items-start">
                 <section className=" col-xl-6 col-md-5 presentacion__myfitteam p-4">
                     <figure className='text-center imagen__logo'>
                         <img src={logoPrincipal} alt="logo" height='200' />
                     </figure>
                    
-                    <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi sit vitae repellat blanditiis minus ratione veritatis qui eligendi, quibusdam quaerat eius, consequatur rem earum rerum! Accusamus magni voluptates obcaecati doloribus.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores natus veniam praesentium, quisquam et blanditiis corrupti id hic atque consequuntur accusantium magni enim deserunt, omnis at similique doloremque assumenda amet.
+                    <p className='text-white'>
+                    Descubre las OFERTAS de los mejores partners y obtén sesiones personalizadas en nutrición & entrenamiento Físico. Encuentra el balance perfecto para ti estés donde estés.   
+                    <br/>
+                    <p className='pt-3'>Conoce sobre nuestras ofertas. Solo debes registrarte</p>
                         </p>
                     <div className='d-flex justify-content-around'>
                         <NavLink to='/nuestros-Profesionales' className='btn btn-outline-warning'>Ver profesionales</NavLink>
